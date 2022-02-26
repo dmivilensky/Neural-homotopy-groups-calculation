15810e47effe60bf891cf90a83060c5a68452a53
kibrq <ki.br178@gmail.com>
2022-02-27 00:04:08 +0300

    Add `is_in_subgroup` method

--- free_group.py
+++ free_group.py
@@ -20,6 +20,50 @@ def reciprocal(word):
     return [-factor for factor in word[::-1]]
 
 
+def normalize(word):
+    _word = []
+    for el in word:
+        if el != 0 and (len(_word) == 0 or _word[-1] != -el):
+            _word.append(el)
+        else:
+            _word.pop()
+    return _word
+
+
+'''
+    Чтобы проверить, что слово w = w_1w_2...w_n лежит в подгруппе образованной словом s = s_1s_2...s_m 
+    можно убрать из w все сдвиги s и s^-1 и проверить, что получился тривиальный элемент
+
+    интуиция:
+        w = yzx, s = xyz. Хотим привести к виду (-a)(xyz)a * (-b)(xyz)b ...
+        w = yzx = yzx y(-y) = yzx y z(-z) (-y) = yz xyz (-z)(-y)
+
+     любой сдвиг операциями домножить на элемент и на обратный ему можно привести к нормальному виду 
+
+'''
+def is_in_subgroup(subgroup, word):
+    _word, doubled_s, doubled_rs = [], subgroup * 2, reciprocal(subgroup) * 2
+
+    def sublist(needle, source):
+        m = len(needle)
+        for idx, el in enumerate(source):
+            # Говорят (https://stackoverflow.com/a/12576755) el == needle[0] ускоряет
+            if el == needle[0] and source[idx:idx+m] == needle:
+                return idx
+        return -1
+    
+    n, m, pointer = len(word), len(subgroup), 0
+    while pointer < n:
+        # s сдвиг t <=> |s| = |t| && s подстрока в t_1t_2...t_nt_1t_2...t_n 
+        if pointer + m <= n and sublist(word[pointer:pointer + m], doubled_s) != -1:
+            pointer += m
+        else:
+            _word.append(word[pointer])
+            pointer += 1
+    
+    return len(normalize(_word)) == 0
+
+
 def conjugation(word, conjugator):
     inverted_conjugator = reciprocal(conjugator)
 
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from free_group import free_group_bounded, normal_closure, reciprocal, conjugation
+from free_group import free_group_bounded, normal_closure, reciprocal, conjugation, is_in_subgroup
 
 a = next(free_group_bounded())
 b = next(free_group_bounded())
@@ -15,3 +15,14 @@ print(conjugation(x, y))
 
 print(next(normal_closure([a])))
 print(next(normal_closure([x])))
+
+def test_is_in_subgroup():
+    subgroup = [1, 2]
+
+    assert is_in_subgroup(subgroup, [1, 2])
+    assert is_in_subgroup(subgroup, [-2, 1, 2, 2])
+    assert is_in_subgroup(subgroup, [2, 1])
+
+    subgroup = [1, 2, 3]
+    assert is_in_subgroup(subgroup, [2, 3, 1])
+    assert is_in_subgroup(subgroup, [-3, 2, 3, 1, 3])
