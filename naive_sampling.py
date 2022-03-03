595e15e3c7b7426bb65ee2180689557685bae727
kibrq <ki.br178@gmail.com>
2022-03-03 11:42:59 +0300

    Move generation of dataset to `generate_dataset.py`

--- naive_sampling.py
+++ naive_sampling.py
@@ -1,37 +1,47 @@
-from data.free_group_generator import generator_from_free_group_bounded
-from data.closure_generator import generator_from_normal_closure_v2 as generator_from_closure
-from core.free_group import normalize, is_in_subgroup, is_in_intersection, wu_numerator_subgroups
-from itertools import combinations
-from pandas import DataFrame
+from core.free_group import free_group_bounded, is_in_subgroup, normalize, is_trivial
+from core.show_word import word_to_string
+from tqdm import tqdm
 
 
-def subgroup_by_class(label):
-    result = []
-    for idx in range(label.bit_length()):
-        if label & (1 << (idx)):
-            result.append(idx + 1)
+def naive_generate_from_intersection(generators_number, max_length, subgroups):
+    word = None
+    while word is None or not all(map(lambda s: is_in_subgroup(s, word), subgroups)):
+        word = next(free_group_bounded(generators_number, max_length))
+    return word
+
+
+def naive_generator(generators_number = 2, max_length = 15):
+    subgroups = [[t] for t in range(1, generators_number + 1)] + [[t for t in range(1, generators_number + 1)]]
+    while True:
+        yield naive_generate_from_intersection(generators_number, max_length, subgroups)
+
+'''
+Function samples words from intersection of <x_1>, <x_2>, ..., <x_1x_2x_3...>
+Method is naive - check whether the random word is in the given intersection
+'''
+def naive_sample(generators_number = 2, max_length = 6, to_sample = 20, distinct = True, verbose = True):
+    '''
+    :param generators_number:   number of generators of embracing group
+    :param max_length:          maximum length of generated words
+    :param to_sample:           number of words to sample
+    :param distinct:            is sampled words should be distintive
+    :param verbose:             use tqdm? 
+    :returns: list of distinct words from the given intersection
+    '''
+    used, generator, result = set(), naive_generator(generators_number, max_length), []
+    for _ in tqdm(range(to_sample)) if verbose else range(to_sample):
+        word = None
+        while word is None or is_trivial(word) or (distinct and tuple(word) in used):
+            word = normalize(next(generator))
+        used.add(tuple(word))
+        result.append(word)
     return result
 
 
-def create_dataset_by_class_distribution(group_index = 2,
-                                        approximate_length = 20,
-                                        approximate_count = 10 ** 6, 
-                                        class_distribution = None):
-    if class_distribution is None:
-        class_distribution = {}
-    undistributed_proba_unit = (1 - sum(class_distribution.values())) / (group_index + 1 - len(class_distribution))
-    for label in range(0, 2 ** group_index):
-        if label not in class_distribution:
-            class_distribution[label] = undistributed_proba_unit
-    result, subgroups = [], wu_numerator_subgroups(group_index)
-    for label in range(0, 2 ** group_index):
-        if label == 0:
-            generator = generator_from_free_group_bounded(group_index, max_length=(1 + 0.2) * approximate_length)
-        else:
-            generator = generator_from_closure(subgroup_by_class(label), group_index, lam=approximate_length)
-        for _ in range(int(approximate_count * class_distribution[label])):
-            word = normalize(next(generator))
-            result.append([word] + [is_in_subgroup(subgroup_by_class(label), word) for label in range(1, 2 ** group_index)] + [is_in_intersection(subgroups, word)])
-
-    columns = ['word'] + [f'is-in-<{",".join(map(str, subgroup_by_class(label)))}>' for label in range(1, 2 ** group_index)] + ['is-in-intersection']
-    return DataFrame(result, columns = columns)
+def write_results(generators_number = 2, max_length = 6, to_sample = 10):
+    results = naive_sample(generators_number=generators_number, max_length=max_length, to_sample=to_sample, distinct=True, verbose=True)
+    from pathlib import Path
+    Path('results', 'naive-sampling').mkdir(parents=True, exist_ok=True)
+    with open(f'results/naive-sampling/{generators_number}-generators-{max_length}-length.txt', 'w') as file:
+        for r in results:
+            file.write(word_to_string(r, inverse_to_string="minus") + '\n')
